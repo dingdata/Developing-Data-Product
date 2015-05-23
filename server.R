8b136493e363211f2d0ecbaa4aaa62eb6c74d498
dingdata <dingbro@gmail.com>
2015-05-23 23:48:31 +0800

    First Commit

---
+++ server.R
@@ -0,0 +1,23 @@
+function(input, output, session) {
+  terms <- reactive({
+    # Change when the "update" button is pressed...
+    input$update
+    # ...but not for anything else
+    isolate({
+      withProgress({
+        setProgress(message = "Processing...")
+        getTermMatrix(input$selection)
+      })
+    })
+  })
+
+  # Make the cloud session
+  wordcloud_rep <- repeatable(wordcloud)
+
+  output$plot <- renderPlot({
+    v <- terms()
+    wordcloud_rep(names(v), v, scale=c(4,0.5),
+                  min.freq = input$freq, max.words=input$max,
+                  colors=brewer.pal(8, "Dark2"))
+  })
+}
